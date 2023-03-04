b3dcb721ceafdc341d662a625268b3bacb23be52
Serge1357 <113466757+Serge1357@users.noreply.github.com>
2023-03-05 01:47:12 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e97x07y3e(jzi9#ger36e!+x095n!$q$ck^h5a6-*!r3bz&k_2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
@@ -32,7 +32,7 @@ DB_PORT = os.environ.get('DB_PORT') or '5432'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite-03.herokuapp.com', ]
 
 
 # Application definition
